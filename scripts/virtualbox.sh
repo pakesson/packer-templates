eafdf7eab7d4ff9cda926ebbe1afb9cdfcbf307a
Philip Åkesson <philip.akesson@gmail.com>
2017-11-08 10:35:45 +0100

    Add user to vboxsf in the virtualbox.sh script

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -5,6 +5,7 @@ set -x # Print all commands as they execute
 
 sudo apt-get -y install dkms
 sudo apt-get -y install make
+sudo apt-get -y install build-essential
 
 # Uncomment this if you want to install Guest Additions with support for X
 #sudo apt-get -y install xserver-xorg
@@ -19,3 +20,6 @@ if [ "$VBOX_VERSION" == '4.3.10' ]; then
   # https://www.virtualbox.org/ticket/12879
   sudo ln -s "/opt/VBoxGuestAdditions-$VBOX_VERSION/lib/VBoxGuestAdditions" /usr/lib/VBoxGuestAdditions
 fi
+
+# Note: This adds the user to the 'vboxsf' group for VirtualBox shared folders
+sudo usermod -G vboxsf -a xubuntu
